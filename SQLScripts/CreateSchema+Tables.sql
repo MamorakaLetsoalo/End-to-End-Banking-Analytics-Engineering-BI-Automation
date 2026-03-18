5ca75f4dca171c93403e88d4e8285897a017d3db
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-03-18 16:37:56 +0200

    Creating warehouse tables

--- SQLScripts/CreateSchema+Tables.sql
+++ SQLScripts/CreateSchema+Tables.sql
@@ -11,3 +11,23 @@ CREATE SCHEMA warehouse;
 
 --Reporting Layer
 CREATE SCHEMA analytics;
+
+--Creating Tables
+--Stating table
+CREATE TABLE staging.stg_transactions (
+    Transaction_ID INT,
+    Customer_ID INT,
+    Account_ID INT,
+    Transaction_Type VARCHAR(50),
+    Channel VARCHAR(50),
+    Currency VARCHAR(10),
+    Amount DECIMAL(18,2),
+    Transaction_Date DATE,
+    Balance_Before DECIMAL(18,2),
+    Balance_After DECIMAL(18,2),
+    Fraud_Flag INT,
+    Risk_Score DECIMAL(5,3),
+    Region VARCHAR(50),
+    Load_Date DATETIME DEFAULT GETDATE());
+     
+    
